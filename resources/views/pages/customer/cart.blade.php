ef8b860e149846193bda435916dd8af6072b110f
Almutawaj <foetheuniversity2022@gmail.com>
2024-01-02 23:34:13 +0300

    Message Indicator

--- resources/views/pages/customer/cart.blade.php
+++ resources/views/pages/customer/cart.blade.php
@@ -114,6 +114,22 @@
                         <span>Total cost</span>
                         <span>BD {{ number_format($totalPrice + 2, 2) }}</span>
                     </div>
+
+                     {{-- message to refresh --}}
+                     <div class="flex items-center p-1 mb-2 text-sm w-full  text-pruple-600 rounded-lg bg-purple-50"
+                     role="alert">
+                     <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                         fill="currentColor" viewBox="0 0 20 20">
+                         <path
+                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                     </svg>
+                     <span class="sr-only">Info</span>
+                     <div>
+                         <span class="font-medium">&nbsp; Refresh to update total cost.</span>
+                     </div>
+
+
+                    </div>
                     <a href="{{ route('customer.checkout') }}">
                         <button
                             class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white w-full <?php echo empty($cart) ? 'bg-gray-400 hover:bg-gray-400 cursor-not-allowed' : ''; ?>"
