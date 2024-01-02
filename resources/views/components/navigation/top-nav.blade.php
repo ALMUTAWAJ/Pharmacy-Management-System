b6b864ae3375e8768b2dc3db2657f543ced24a65
Almutawaj <foetheuniversity2022@gmail.com>
2024-01-03 00:46:19 +0300

    More Modification

--- resources/views/components/navigation/top-nav.blade.php
+++ resources/views/components/navigation/top-nav.blade.php
@@ -70,9 +70,9 @@
                     <li>
                       <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Orders</a>
                     </li>
-                    <li>
+                    {{-- <li>
                       <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Payment methods</a>
-                    </li>
+                    </li> --}}
                     <li>
                       <a href="{{route('logout')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                     </li>
